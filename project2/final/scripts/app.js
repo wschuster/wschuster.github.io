f934d662af53de3bc79b22946212e4a33a402f25
wschuster <wschuster@collegeforcreativestudies.edu>
2016-11-06 21:03:06 -0500

    kermission complete

--- project2/final/scripts/app.js
+++ project2/final/scripts/app.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
         type: 'GET',
         //insert your the url of your JSON file hosted by github
 
-        url: 'https://raw.githubusercontent.com/JarenTigani/JarenTigani.Github.io/master/Project_2/scripts/data.json',
+        url: 'https://raw.githubusercontent.com/wschuster/wschuster.github.io/master/project2/final/scripts/data.json',
         dataType: 'json',
 
         success: function(data) {
